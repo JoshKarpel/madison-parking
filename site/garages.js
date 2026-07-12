773210339dcade83ae84294dab6955d7dce8c5c8
Josh Karpel <josh.karpel@gmail.com>
2026-07-12 01:13:20 -0500

    WIP

--- site/garages.js
+++ site/garages.js
@@ -1,10 +1,10 @@
 // Addresses are the city's official garage addresses (all Madison, WI 53703),
 // used to build a Google Maps link that points directly at the garage.
 export const GARAGES = {
-  1:  { name: "Overture Center",      address: "318 W. Mifflin St., Madison, WI 53703" },
+  1:  { name: "Overture Center",      address: "318 W. Mifflin St., Madison, WI 53703", note: "Near the Overture Center & Library" },
   2:  { name: "State Street Capitol", address: "200 N. Carroll St., Madison, WI 53703", note: "Near Ian's Pizza" },
-  5:  { name: "State Street Campus",  address: "430 N. Frances St., Madison, WI 53703" },
+  5:  { name: "State Street Campus",  address: "430 N. Frances St., Madison, WI 53703", note: "Near Estacion Inka" },
   6:  { name: "Capitol Square North", address: "218 E. Mifflin St., Madison, WI 53703", note: "Near Heritage Tavern" },
   18: { name: "South Livingston St",  address: "111 S. Livingston St., Madison, WI 53703", note: "Near The Sylvee" },
-  19: { name: "Wilson Street",        address: "20 E. Wilson St., Madison, WI 53703" },
+  19: { name: "Wilson Street",        address: "20 E. Wilson St., Madison, WI 53703", note: "Near Monona Terrace" },
 };
